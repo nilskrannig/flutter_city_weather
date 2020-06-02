d4cdb5c316980b561972581c4af57999cc793aa7
Nilsferd <nilskrannig@gmail.com>
2020-06-02 11:11:53 +0200

    init weather project with location

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:nilskrannig/services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+}
